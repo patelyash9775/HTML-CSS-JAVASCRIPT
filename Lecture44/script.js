5efdf33e453c5bb9aee04d222365f5bc56bbb8d2
Patel Yash Pankajbhai <71980446+patelyash9775@users.noreply.github.com>
2021-03-29 21:50:27 +0530

    Default values in Function

---
+++ Lecture44/script.js
@@ -0,0 +1,11 @@
+function orderChickenwith(sideDish) {
+
+	sideDish = sideDish || "Whatever!";
+	/*if(sideDish==undefined) {
+		sideDish="Whatever!";
+	}*/
+	console.log("Chicken with " + sideDish);
+}
+
+orderChickenwith("noodles");
+orderChickenwith();
