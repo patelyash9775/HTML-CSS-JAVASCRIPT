9ec485457ec7b1caf7f1a27895253cbafa19d1a2
Patel Yash Pankajbhai <71980446+patelyash9775@users.noreply.github.com>
2021-03-29 22:03:22 +0530

    object literals and this

---
+++ Lecture49/script.js
@@ -0,0 +1,32 @@
+//object literals and "this"
+
+/*var literalCircle= {
+		radius: 10,
+
+		getArea: function () {
+			console.log(this);
+			return Math.PI * Math.pow(this.radius,2);
+		}
+	};
+
+console.log(literalCircle.getArea());  */
+
+var literalCircle = {
+	radius: 10,
+
+	getArea: function() {
+		var self = this;
+		console.log(this);
+
+		var increaseRadius = function () {
+			self.radius = 20;
+
+		};
+		increaseRadius();
+		console.log(this.radius);
+
+		return Math.PI * Math.pow(this.radius,2);
+	}
+};
+
+console.log(literalCircle.getArea());
